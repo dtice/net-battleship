e175511e87bdf418c8a84fd76b09738f35f371e2
dtice <dillon.tice@gmail.com>
2018-09-12 13:57:54 -0600

    Basic Server Implementation

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-import http.server
+from http.server import *
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
+    print("Running " + str(httpd.server_name) + " on " + str(httpd.server_port) + "...")
     httpd.serve_forever()
+
+run()
