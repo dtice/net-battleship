f5d6c1617716845a1601fd54625b19912371122b
dtice <dillon.tice@gmail.com>
2018-09-12 16:02:13 -0600

    Simple Socket Boilerplate

--- client.py
+++ client.py
@@ -1,1 +1,17 @@
-import http.client
+import socket
+import sys
+
+arg = sys.argv
+port = 8000
+
+target = '127.0.0.1'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target, port))
+
+client.send('{};{}'.format(arg[1], arg[2]))
+
+response = client.recv(4096)
+
+print response
--- server.py
+++ server.py
@@ -1,9 +1,38 @@
-from http.server import *
+# Simple WebServer Relative to Current Directory
+# from http.server import *
+# from socketserver import *
+#
+#
+# def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+#     server_address = ('', 8000)
+#     httpd = server_class(server_address, handler_class)
+#     print("Running " + str(httpd.server_name) + " on " + str(httpd.server_port) + "...")
+#     httpd.serve_forever()
+# run()
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    print("Running " + str(httpd.server_name) + " on " + str(httpd.server_port) + "...")
-    httpd.serve_forever()
+import socket
+import threading
 
-run()
+bind_addr = '127.0.0.1'
+bind_port = 8000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_addr, bind_port))
+server.listen(1)
+
+print("Listening on ", bind_addr, " : ", bind_port)
+
+def handle_client_connection(client_socket):
+    request = client_socket.recv(1024)
+    x, y = str(request).split(';')
+    client_socket.send('{}*{}={}'.format(x, y, int(x)*int(y))
+    client_socket.close()
+
+while True:
+    client_sock, address = server.accept()
+    print("Accepted connection from ", address[0], ":", address[1])
+    client_handler = threading.Thread(
+        target=handle_client_connection,
+        args=(client_sock,)
+    )
+    client_handler.start()
