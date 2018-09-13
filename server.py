1c92e5de40357e5aa532074fd36c177679e89e00
Dillon Tice <dtice@localhost.localdomain>
2018-09-12 20:13:52 -0600

    Commenting

--- server.py
+++ server.py
@@ -1,29 +1,24 @@
-# Simple WebServer Relative to Current Directory
-# from http.server import *
-# from socketserver import *
-#
-#
-# def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-#     server_address = ('', 8000)
-#     httpd = server_class(server_address, handler_class)
-#     print("Running " + str(httpd.server_name) + " on " + str(httpd.server_port) + "...")
-#     httpd.serve_forever()
-# run()
 from http.server import *
 from http.client import *
 import sys
 import requests
 
+# get port and name of board file from arguments
 port = int(sys.argv[1])
 board = sys.argv[2]
 
+# client_handler uses BaseHTTPRequestHandler to handle POST requests
 class client_handler(BaseHTTPRequestHandler):
     def _set_headers(self, hit, sunk, dupe, ib):
+        # if the player has never hit this spot
         if dupe == 0:
+            # if the salvo is in-bounds
             if ib == 1:
+                # if the salvo hits a boat
                 if hit == 1:
                     self.send_response(200)
                     self.send_header('hit', '1')
+                    # if the salvo sinks a boat
                     if sunk != 'X':
                         self.send_header('sunk',sunk)
                 else:
@@ -38,23 +33,38 @@ class client_handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_POST(self):
-        s = int(self.headers.get_all('content-length')[0])
+        # gets the length of the custom headers
+        s = int(self.headers.get('content-length')[0])
+        # reads the custom headers
         coords = self.rfile.read(s).decode('utf-8')
+
+        # string manipulation
         y,x = coords.split("&")
         x = x.split("=")[1]
         y = y.split("=")[1]
-        print("Received coordinates: X: {}, Y: {}".format(x,y))
+
+        # prints coordinates
+        # print("Received coordinates: X: {}, Y: {}".format(x,y))
+
+        # checks board using given coordinates
         hit,sunk,dupe,ib = check_board(x,y)
+
+        # sets headers on packet
         self._set_headers(hit,sunk,dupe,ib)
 
+# checks board and sets hit, sunk, dupe, and ib according to coordinates and boat placement
 def check_board(x, y):
     hit,sunk,dupe,ib = 1, 'D', 0, 1
     return hit,sunk,dupe,ib
 
+# runs the server
 def run(server_class=HTTPServer, handler_class=client_handler, port=port):
+    # sets server address and port
     server_address = ('127.0.0.1', port)
+    # initializes server class
     httpd = server_class(server_address, handler_class)
     print('Starting server on port ', port)
+    # starts server
     httpd.serve_forever()
 
 run()
